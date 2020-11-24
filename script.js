c9b3a9a330b7068a58fe5c4458884e48896939aa
Nikola <nikola.tomasovic@medieinstitutet.se>
2020-11-24 11:22:35 +0100

    added more

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// vi vill skapa info om en person. ex namn, ålder, kön osv.
+// Vi vill skapa info om en person. Till exempel namn, ålder, kön osv.
 let name = "Niklas";
-let age = 36;
-let gender = "Male"
+let age = 30;
+let gender = "Male";
 
 let name1 = "Maria";
 let age1 = 28;
 let gender1 = "Female";
 
-// Istället för att skapa massa enskilda variabler så kan vi klumpa ihop de variablerna som hör ihop till ett och samma objekt.
+// Istället för att skapa massa enskilda variabler för olika personer, så kan vi klumpa ihop de variablerna som hör ihop till ett och samma objekt.
 
 let niklas = {
-    name: "Niklas",
-    age: 30,
-    gender: "Male",
+  name: "Niklas",
+  age: 30,
+  gender: "Male",
 };
 
 let maria = {
-    name: "Maria",
-    age: 28,
-    gender: "Female",
+  name: "Maria",
+  age: 28,
+  gender: "Female",
 };
 
-console.log(niklas, maria);
+console.log(niklas, maria)
 
-// Vi kan lägga till egenskaper på ett simpelt sätt någon av utav objekten. Det gör vi enklast genom punknotation.
+// Vi kan lägga til egenskaper på ett simpelt sätt till något av utav objekten. Det gör vi enklast genom punktnotation.
 niklas.address = "Drottninggatan 20";
 
 // Vi kan även ändra på egenskaper i ett objekt. Det finns två sätt att göra detta på. Första är via punknotation.
@@ -31,10 +31,10 @@ niklas.age = 31;
 
 // Det andra sättet är på ett arrayliknande sätt.
 niklas["age"] = 45;
-niklas["gender"] = "non-binary";
+niklas["gender"] = "Non-binary";  
 
-// Vill vi ta borg en egenskap, vilket man sällan gör, istället brukar man sätta värdet till null eller 0 (om det är ett nummer) eller en tom sträng om det är en sträng.
+// Vill vi ta bort en egenskap, vilket man sällan gör, istället brukar man sätta värdet till null eller 0 (om det är ett nummer) eller en tom sträng om det är en sträng.
 delete niklas.age;
-delete niklas["address"]
+delete niklas["address"];
 
-console.log(niklas);
+console.log(niklas, maria);
--- store.js
+++ store.js
@@ -1,57 +1,91 @@
-// ----------- store-object ----------
+// ---------- store object ----------
+
 let ica = {
     name: "Ica Supermarket Skurup",
     address: "Drottninggatan 29",
-    zipcod: "12389",
+    zipcode: "12389",
     city: "Skurup",
-    staff: [],
+    staff: [], // Defautlvärdet är en tom array som vi sen kan lägga till ta bort ifrån med hjälp av javascript.
     products: [],
-    hireStaff: function (person){
-        // Vi andvänder oss av "this" här, för att "this" hänvisar till det egna objektet. Så "this" = ica
-        this.staff.push(person);
-        person.worksAt = this;
+    hireStaff: function (person) {
+      // Vi använder oss av "this" här, för att "this" hänvisare till det egna objektet. Så "this" = ica
+      this.staff.push(person);
+      person.worksAt = this;
+      return this;
+    },
+    fireStaff: function (person) {
+      // Vi kan här använder en arraymetod som heter filter(), den filtrerar bort element utifrån ett villkor. Är villkoret sant, så behålls elementet. Är villkoret falskt så sorteras det bort.
+      this.staff = this.staff.filter((p) => {
+          return p !== person;
+      });
+      return this;
+    },
+    addProduct: function (product) {
+      this.products.push(product)
+      return this;
     }
-    fireStaff: function(person){
-        // vi kan här använda en arraymetod som heter filter(). den filtrerar bort element utifrån ett villkor. är villkoret sant, så behålls elementet. är dvillkoret falskt så sorteras det bort.
-        this.staff = this.staff.filter(p => p !== person);
-        return this;
-    }
-};
-
-// ---------- person-objects -----------
-let niklas = {
+  };
+  
+  // ---------- person objects ----------
+  
+  let niklas = {
     firstName: "Niklas",
     lastName: "Håkansson",
     age: 30,
     salary: 45000,
-    worksAt: null,
-    startWork: function(store){
-        this.worksAt = store;
-        store.staff.push(this);
-    }
-};
-
-let erik = {
+    worksAt: null, // Vi sätter null som ett standarvärde när personen inte har en arbetsplats.
+    // Denna metod lägger till en arbetsplats på personobjektet och uppdaterar även storeibjektes staff-array med personen.
+    startWorkAt: function (store) { 
+      this.worksAt = store;        
+      store.staff.push(this);
+      return this;
+    },
+  };
+  
+  let erik = {
     firstName: "Erik",
     lastName: "Jansson",
     age: 45,
     salary: 23000,
     worksAt: null,
-    startWork: function(store){
-        this.worksAt = store;
-        store.staff.push(this);
-    }
-};
-
-let sofia = {
+    startWorkAt: function (store) {
+      this.worksAt = store;
+      store.staff.push(this);
+      return this;
+    },
+  };
+  
+  let sofia = {
     firstName: "Sofia",
     lastName: "Karlsson",
     age: 22,
     salary: 60000,
     worksAt: null,
-    startWork: function(store){
-        this.worksAt = store;
-        store.staff.push(this);
-    }
-};
-
+    startWorkAt: function (store) {
+      this.worksAt = store;
+      store.staff.push(this);
+      return this;
+    },
+  };
+  
+  // ---------- product objects ----------
+  
+  let milk = {
+    name: "Mellanmjök",
+    price: 11,
+  }
+  
+  let cheese = {
+    name: "Herrgårdsost",
+    price: 59,
+  };
+  
+  let beef = {
+    name: "Biff",
+    price: 109,
+  };
+  
+  let fish = {
+    name: "Lax",
+    price: 89,
+  };
