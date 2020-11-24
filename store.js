79f85b1e3249667803917772eb7727fa32c4a429
Nikola <nikola.tomasovic@medieinstitutet.se>
2020-11-24 10:00:43 +0100

    added store.js with som new examples

---
+++ store.js
@@ -0,0 +1,34 @@
+// ----------- store-object ----------
+let ica = {
+    name: "Ica Supermarket Skurup",
+    address: "Drottninggatan 29",
+    zipcod: "12389",
+    city: "Skurup",
+    staff: [],
+    products: [],
+};
+
+// ---------- person-objects -----------
+let niklas = {
+    firstName: "Niklas",
+    lastName: "Håkansson",
+    age: 30,
+    salary: 45000,
+    worksAt: {},
+};
+
+let erik = {
+    firstName: "Erik",
+    lastName: "Jansson",
+    age: 45,
+    salary: 23000,
+    worksAt: {},
+};
+
+let sofia = {
+    firstName: "Sofia",
+    lastName: "Karlsson",
+    age: 22,
+    salary: 60000,
+    worksAt: {},
+};
